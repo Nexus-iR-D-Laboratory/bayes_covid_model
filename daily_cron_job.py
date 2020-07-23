5bf06b45d5fe586226c9fb51b59b0419876434d0
Philip Smith <pas147@case.edu>
2020-07-23 18:38:29 -0400

    Update daily_cron_job.py

--- daily_cron_job.py
+++ daily_cron_job.py
@@ -11,51 +11,19 @@ from sub_units.utils import Region
 from sub_units import post_analysis
 from sub_units import github_readme_components
 from sub_units.utils import print_and_write
-
-# TODO: re-implement sucking data from the internet by checking for all days
-#   and sucking only what it needs and put that in the load_data module
-#   so it automatically happens whenever you load the data, rather
-#   than having to manually do it here.
-
-#####
-# Step 1a: Update counts data
-#####
-
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
-r = requests.get(url, allow_redirects=True)
-with open('source_data/states.csv', 'w') as f:
-    f.write(r.content.decode("utf-8"))
-
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-r = requests.get(url, allow_redirects=True)
-with open('source_data/counties.csv', 'w') as f:
-    f.write(r.content.decode("utf-8"))
-
-print('Downloading last week of data')
-for days_back in tqdm(range(0, 7)):
-    date = datetime.date.today() - datetime.timedelta(days=days_back)
-    date_str = date.strftime('%m-%d-%Y')
-    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date_str}.csv"
-    r = requests.get(url, allow_redirects=True)
-    filename = f'source_data/csse_covid_19_daily_reports/{date_str}.csv'
-    print(filename, len(r.content.decode("utf-8")))
-    with open(filename, 'w') as f:
-        f.write(r.content.decode("utf-8"))
-
-#####
-# Step 1b: Update load_data (this happens as soon as you import modules that use load_data)
-#####
-
 import datetime
 import covid_moving_window as covid
+from sub_units import post_analysis
+
+# get today's date
+yesterdays_date_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+print(f'Yesterday: {yesterdays_date_str}')
+
 
 #####
 # Step 2: Run Update
 #####
 
-# get today's date
-yesterdays_date_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-print(f'Yesterday: {yesterdays_date_str}')
 
 covid.n_bootstraps = None
 covid.n_likelihood_samples = None
@@ -96,9 +64,22 @@ print(region_plot_subfolders)
 # Step 2c: post-process parameters to identify which regions to deep-dive into
 ####
 
+# Copy from AWS to local directory...
+# HYP_STR=2020_06_17_date_smoothed_moving_window_21_days_US_counties_region_statsmodels; mkdir $HYP_STR; aws s3 cp --recursive s3://covid-figures/$HYP_STR/ $HYP_STR
+# HYP_STR=2020_06_17_date_smoothed_moving_window_21_days_US_states_region_statsmodels; mkdir $HYP_STR; aws s3 cp --recursive s3://covid-figures/$HYP_STR/ $HYP_STR
+# HYP_STR=2020_06_17_date_smoothed_moving_window_21_days_countries_region_statsmodels; mkdir $HYP_STR; aws s3 cp --recursive s3://covid-figures/$HYP_STR/ $HYP_STR
+# HYP_STR=2020_06_17_date_smoothed_moving_window_21_days_provinces_region_statsmodels; mkdir $HYP_STR; aws s3 cp --recursive s3://covid-figures/$HYP_STR/ $HYP_STR
+
+region_plot_subfolders = {Region.US_counties: '2020_06_17_date_smoothed_moving_window_21_days_US_counties_region_statsmodels', 
+Region.countries: '2020_06_17_date_smoothed_moving_window_21_days_countries_region_statsmodels',
+Region.US_states: '2020_06_17_date_smoothed_moving_window_21_days_US_states_region_statsmodels'}
+
 post_analysis.hyperparameter_strings = list(region_plot_subfolders.values())
 
-from sub_units import post_analysis
+####
+# Step 2c: post-process parameters to identify which regions to deep-dive into
+####
+
 
 post_analysis.post_process_state_reports(opt_acc=True)
 
@@ -118,8 +99,6 @@ region_plot_subfolders = covid.run_everything()
 # Step 2d: post-process parameters to identify which regions to deep-dive into
 ####
 
-from sub_units import post_analysis
-
 post_analysis.post_process_state_reports(opt_acc=False)
 
 # re-run and produce all the plots for the top outbreak-y counties
@@ -134,11 +113,10 @@ with open(post_analysis.scratchpad_filename, 'r') as f:
 
 region_plot_subfolders = covid.run_everything()
 
-# TODO: Make update to README automatic
+#####
+# Step 2e: Re-do post-analysis and write to github table file, even if you downloaded results from the server
+#####
 
-#####
-# Step 2e: Re-do post-analysis and write to github table file
-#####
 
 from sub_units import post_analysis
 from sub_units import github_readme_components
@@ -153,8 +131,6 @@ print_and_write(github_readme_components.get_all(github_table_filename=post_anal
 
 shutil.copyfile('github_README.txt', 'README.md')
 
-
-# TODO: Copy 'github_README.txt' to 'README.md'
 
 #####
 # Step 3: Upload Figures to AWS
@@ -179,15 +155,17 @@ from sub_units.utils import Region
 import generate_plot_browser_moving_window_statsmodels_only as generate_figure_browser
 
 opt_file_check = False  # change to False after run on server
+date_str = '2020_06_07'
 
 region_plot_subfolders = {
     # Region.provinces: 'state_plots/2020_05_02_date_smoothed_moving_window_21_days_provinces_region_statsmodels',
-    Region.countries: 'state_plots/2020_06_02_date_smoothed_moving_window_21_days_countries_region_statsmodels',
-    Region.US_states: 'state_plots/2020_06_02_date_smoothed_moving_window_21_days_US_states_region_statsmodels',
-    Region.US_counties: 'state_plots/2020_06_02_date_smoothed_moving_window_21_days_US_counties_region_statsmodels'
+    Region.countries: 'state_plots/{date_str}_date_smoothed_moving_window_21_days_countries_region_statsmodels',
+    Region.US_states: 'state_plots/{date_str}_date_smoothed_moving_window_21_days_US_states_region_statsmodels',
+    Region.US_counties: 'state_plots/{date_str}_date_smoothed_moving_window_21_days_US_counties_region_statsmodels'
 }
 
 for region, plot_subfolder in region_plot_subfolders.items():
+    
     hyperparamater_str = os.path.basename(os.path.normpath(plot_subfolder))
     print(hyperparamater_str)
 
@@ -205,9 +183,9 @@ for region, plot_subfolder in region_plot_subfolders.items():
         regions_to_present = [f for f in os.listdir(data_dir) if not os.path.isfile(os.path.join(data_dir, f))]
 
     print(sorted(regions_to_present))
-
+    
     # Regenerate Figures
-    generate_figure_browser.hyperparameter_str = hyperparamater_str + '/'
+    generate_figure_browser.hyperparameter_str = hyperparamater_str.replace(date_str, 'current') + '/'
     generate_figure_browser.plot_browser_dir = f'plot_browser_moving_window_statsmodels_only_{region}'
     generate_figure_browser.regions_to_present = regions_to_present
     generate_figure_browser.generate_plot_browser(regions_to_present)
@@ -218,7 +196,7 @@ for region, plot_subfolder in region_plot_subfolders.items():
 
 import os
 import shutil 
-date_str = yesterdays_date_str
+date_str = yesterdays_date_str.replace('-','_')
 
 map_filename_to_github = {
     os.path.join('state_plots',
@@ -257,7 +235,25 @@ for in_file, out_file in map_filename_to_github.items():
     dest_filename = shutil.copyfile(in_file, out_file)
 
 ######
-# Step 5: Push to Github
+# Step 5: Update Choropleths
+######
+
+from sub_units import post_analysis
+
+date_str = '2020_06_09'
+
+post_analysis.hyperparameter_strings = [os.path.basename(os.path.normpath(x)).format(date_str=date_str) for x in region_plot_subfolders.values()]
+post_analysis.post_process_state_reports(opt_acc=False, opt_reset_tables_file=True)
+post_analysis.post_process_state_reports(opt_acc=True)
+
+post_analysis.countries_hp_str = [x for x in post_analysis.hyperparameter_strings if 'countries' in x][0]
+post_analysis.counties_hp_str = [x for x in post_analysis.hyperparameter_strings if 'US_counties' in x][0]
+post_analysis.states_hp_str = [x for x in post_analysis.hyperparameter_strings if 'US_states' in x][0]
+
+post_analysis.choropleth_test()
+
+######
+# Step 6: Push to Github
 ######
 
 # Make sure all the folders are in the github repo
